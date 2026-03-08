4eae3d6f6df969dae50330c50da4a7ab369500de
Nehagowda06 <arcaakvira@gmail.com>
2026-03-08 22:31:27 +0530

    Fix Next.js 16 config and hero svg

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -53,10 +53,11 @@ export default function Hero() {
 
         <div className="bg-[#d9ddf6] p-10 rounded-3xl shadow-xl flex justify-center">
 
-          <img
-            src="/hero.svg"
-            className="max-w-xs"
-          />
+	 <img
+  	   src="/hero.svg"
+  	   alt="Developer illustration"
+  	   className="max-w-xs w-full"
+	 />
 
         </div>
 
